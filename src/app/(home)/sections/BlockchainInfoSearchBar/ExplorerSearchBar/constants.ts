2710ad8a63ff2895e7a47531d3d822e67bb91436
ushkoff <lol454ushkbeavise@gmail.com>
2024-08-08 21:36:43 +0200

    Updated placeholders in ExplorerSearchBar

--- src/app/(home)/sections/BlockchainInfoSearchBar/ExplorerSearchBar/constants.ts
+++ src/app/(home)/sections/BlockchainInfoSearchBar/ExplorerSearchBar/constants.ts
@@ -9,8 +9,8 @@ export const FILTER_OPTIONS = {
 
 export const PLACEHOLDER_TEXT = {
   [FILTER_OPTIONS.CHOOSE]: '<-- Apply a filter to look up for a TXID, SID, Block ID, Pool ID or Contract ID',
-  [FILTER_OPTIONS.TRANSACTIONS]: 'Enter Transaction ID (TXID)',
-  [FILTER_OPTIONS.SHARDS]: 'Enter Shard ID (SID)',
+  [FILTER_OPTIONS.TRANSACTIONS]: 'Enter the 256 bit BLAKE3 hash of transaction',
+  [FILTER_OPTIONS.SHARDS]: 'Enter the SID in format <shard>:<index>',
   [FILTER_OPTIONS.BLOCKS]: 'Enter Block ID',
   [FILTER_OPTIONS.POOLS]: 'Enter Pool ID',
   [FILTER_OPTIONS.CONTRACTS]: 'Enter Contract ID',
