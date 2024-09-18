685e677168d156c2a1a8436cfdd3e3d8896248b7
ushkoff <lol454ushkbeavise@gmail.com>
2024-09-18 11:33:10 +0200

    test commit

--- src/app/pools/[id]/page.tsx
+++ src/app/pools/[id]/page.tsx
@@ -36,7 +36,6 @@ export default async function PoolByIdPage({ params }: PageProps) {
         }}
         items={[
           <ContentBlock key='pool_id' title='Pool Id:' value={poolId}/>,
-          
           <ContentBlock key='quorum_member_status' title='In current quorum:'>
             <Label variant={pool.isCurrentQuorumMember? 'green' : 'red'}>{pool.isCurrentQuorumMember ? 'Yes' : 'No'}</Label>
           </ContentBlock>,
