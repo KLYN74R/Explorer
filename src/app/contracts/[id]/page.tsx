2890bada8b6ee4e15af6a6082a6691249d4c265a
VladChernenko <vladchernenko@protonmail.com>
2024-09-28 22:15:07 +0300

    Added explanation for system contract shard location

--- src/app/contracts/[id]/page.tsx
+++ src/app/contracts/[id]/page.tsx
@@ -59,7 +59,7 @@ export default async function ContractByIdPage({ params }: PageProps) {
         }}
         items={[
           <ContentBlock key='contract_id' title='Contract Id:' value={contractId}/>,
-          <ContentBlock key='shard' title='Shard:' value={shardId}/>,
+          <ContentBlock key='shard' title='Shard:' value={shardId} comment={shardId === 'x' ? 'system contracts are not tied to any shard' : ''}/>,
           [
             <ContentBlock key='balance' title='Balance:' value={contract.balance + ' KLY'}/>,
             <ContentBlock key='uno' title='UNO:' value={contract.uno}/>
